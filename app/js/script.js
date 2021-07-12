47dbf823d0265181aea994f9a82e4601330971e3
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-07-13 01:06:46 +0200

    mobile menu in progress

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', () => {
+    header.classList.contains('open') ? (header.classList.remove('open'), overlay.classList.remove('fade-in'), overlay.classList.add('fade-out')) : (header.classList.add('open'), overlay.classList.add('fade-in'), overlay.classList.remove('fade-out'));
+});
